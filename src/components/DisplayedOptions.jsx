12268195c73e06ab8f086aef28eb138003622dd3
Mitch <70587519+mitchellschul@users.noreply.github.com>
2024-03-14 00:53:58 -0400

    a lil spice

--- src/components/DisplayedOptions.jsx
+++ src/components/DisplayedOptions.jsx
@@ -5,12 +5,14 @@ function DisplayedOptions({ businesses, isOpen, setIsOpen, setRestaurantLink }) 
   console.log(businesses, "BUSINESSES")
 
   return (
-    <div className="grid grid-cols-1 gap-4 w-screen overflow-auto max-h-[55rem] overflow-x-hidden ">
-      {businesses.map((business, index) => (
-        <BusinessCardDisplay key={index} business={business} setIsOpen={setIsOpen} setRestaurantLink={setRestaurantLink}/>
-      ))}
+    <div className="grid grid-cols-1 gap-4 overflow-auto max-h-[55rem] overflow-x-hidden">
 
-    </div>
+      {businesses.map((business, index) => (
+        <BusinessCardDisplay key={index} business={business} setIsOpen={setIsOpen} setRestaurantLink={setRestaurantLink} />
+      ))}
+      <div className="absolute bottom-[284px] left-[114px] right-0 h-28  w-[850px] bg-gradient-to-b from-transparent to-gray-200"></div>
+
+    </div >
   );
 }
 
