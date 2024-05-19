56abd3f05754a629cac7c330ed2558eefb2161f7
Aaron Perel <36312272+aabron@users.noreply.github.com>
2024-05-19 16:28:24 -0400

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -34,8 +34,8 @@ function Landing({ setIsHotelSpecific, disabled }) {
       <div style={{'--image-url': `url(${fetchedUrl})`}} className='h-[90vh] mt-[-100px] w-full bg-[image:var(--image-url)] bg-cover flex justify-center items-center transition-all ease-in-out duration-500'>
           <div className={'flex justify-center items-center mb-[672px]'}>
               {/* <button disabled={disabled} onClick={openFullscreen} className='bg-red-500 px-2 py-3 text-6xl top-96 text-white absolute disabled:opacity-0'>Full Screen </button> */}
-              <div className='absolute h-[29%] w-[70%] mx-auto rounded-xl bg-gradient-to-b from-black via-black to-black opacity-90'></div>
-              <div className='absolute h-[35%] w-[70%] mx-auto rounded-xl bg-gradient-to-t from-black via-transparent to-transparent'></div>
+              <div className='absolute h-[32%] w-[70%] mx-auto rounded-xl bg-gradient-to-b from-black via-black to-black opacity-90'></div>
+              <div className='absolute h-[32%] w-[70%] mx-auto rounded-xl bg-gradient-to-t from-black via-transparent to-transparent'></div>
               <div className='flex justify-center items-center flex-col h-full w-[100%] text-center'>
                   <h1 className='text-white font-quicksand z-10 text-7xl mb-10'>Explore</h1>
                   <h1 className='text-white font-cursive z-10 text-9xl flex flex-row'> Winter Park</h1>
